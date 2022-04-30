67f7903986f3523aaa1bdd035fd98a00d31b2f0f
LeeHwaRang <snow2271@gmail.com>
2022-04-30 21:16:29 +0900

    Setting :wrench: 초기세팅

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.get('/', function(req, res){
-    res.send('hello NodeJs');
-})
-app.listen(4000, () => console.log('4000번 포트 대기'));
+app.use(cors());
+
+app.get("/", function (req, res) {
+  res.send("hello NodeJs");
+});
+app.listen(4000, () => console.log("4000번 포트 대기"));

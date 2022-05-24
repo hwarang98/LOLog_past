39b8389eaf812b3da0495c28d3139b7156d946c8
LeeHwaRang <snow2271@gmail.com>
2022-05-25 01:38:04 +0900

    소환사정보 화면 출력

--- client/src/components/summoner.js
+++ client/src/components/summoner.js
@@ -1,60 +1,66 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useLocation } from "react-router";
 
 function Summoners() {
-  const [userInfo, setUserInfo] = useState("");
-  const [id, setId] = useState(
-    "OBHZRCKHtthQBE3HZR2ToLk4hX5NJsUetw35yTGfIx7c1hPDRjlq10gVlQ"
-  );
-  const [accountId, setAccountId] = useState(
-    "jAzfVRgCfRkdDxVYs9cVB7TFdQMVLqdrgme-qRZ0Opz3-vD-sP_t_2qj"
-  );
-  const [puuid, setPuuid] = useState(
-    "_4GR53i0CNAsi64X7fepBN0QIZhSkiEgO1QK_hxyIJdEnGS4pzr1VRatAAj0svbs78JXtOaJZy5zuQ"
-  );
-  const [name, setName] = useState("돌면킬");
-  const [profileIconId, setProfileIconId] = useState(4837);
-  const [revisionDate, setRevisionDate] = useState(1652591122000);
-  const [summonerLevel, setsummonerLevel] = useState(84);
-
+  const { state } = useLocation();
+  const [userInfo, setUserInfo] = useState();
+  const {
+    id,
+    accountId,
+    puuid,
+    name,
+    profileIconId,
+    revisionDate,
+    summonerLevel,
+  } = state;
   const userApi = (e) => {
-    // e.preventDefault();
     console.log("검색결과인데 들어옴?");
     const url = "http://localhost:4000/summoner";
     axios
       .post("http://localhost:4000/summoner", {
-        id: id,
-        accountId: accountId,
-        puuid: puuid,
-        profileIconId: profileIconId,
-        revisionDate: revisionDate,
-        summonerLevel: summonerLevel,
+        id,
+        accountId,
+        puuid,
+        name,
+        profileIconId,
+        revisionDate,
+        summonerLevel,
       })
       .then((data) => {
-        console.log(data.data);
-        setUserInfo(data.data);
+        const userData = data.data.data;
+        // console.log(userData);
+        setUserInfo(userData);
         console.log("성공");
       })
       .catch((err) => console.log("에러뜸: ", err));
   };
-  // useEffect(() => {
-  //   userApi();
-  // });
+  console.log(userInfo);
+  useEffect(() => {
+    return () => {
+      userApi();
+    };
+  }, []);
   return (
     <div>
       <h1>소환사 정보</h1>
-      <button onClick={userApi}>ddddd</button>
+      {userInfo &&
+        userInfo.map((user) => {
+          console.log("user?: ", user.summonerName);
+          return (
+            <div>
+              <li>{user.summonerName}</li>
+              <li>
+                {user.tier} {user.rank}
+              </li>
+              <li>{user.wins}승</li>
+              <li>{user.losses}패</li>
+              {user.queueType}
+            </div>
+          );
+        })}
     </div>
   );
 }
 
 export default Summoners;
-// {
-//   "id": "OBHZRCKHtthQBE3HZR2ToLk4hX5NJsUetw35yTGfIx7c1hPDRjlq10gVlQ",
-//   "accountId": "jAzfVRgCfRkdDxVYs9cVB7TFdQMVLqdrgme-qRZ0Opz3-vD-sP_t_2qj",
-//   "puuid": "_4GR53i0CNAsi64X7fepBN0QIZhSkiEgO1QK_hxyIJdEnGS4pzr1VRatAAj0svbs78JXtOaJZy5zuQ",
-//   "name":"'돌면킬",
-//   "profileIconId": 4837,
-//   "revisionDate": 1652591122000,
-//   "summonerLevel": 84
-//   }
